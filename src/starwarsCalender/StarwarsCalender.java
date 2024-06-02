087f4dbedd7cd271127f3493c565130be5891e83
Nam-Cheol <namcher9428@gmail.com>
2024-06-02 20:37:37 +0900

    Initial commit

---
+++ src/starwarsCalender/StarwarsCalender.java
@@ -0,0 +1,48 @@
+package starwarsCalender;
+
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class StarwarsCalender extends JFrame{
+
+	private JLabel backgroundMap;
+	private SystemTray tray;
+	private TrayIcon icon;
+	
+	public StarwarsCalender() {
+		initData();
+		setInitLayout();
+		addEventListener();
+	}
+	
+	private void initData() {
+		
+		backgroundMap = new JLabel(new ImageIcon("/image/test.png"));
+		
+		setTitle("임찬님, Starwars에 오신 걸 환영합니다.");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(backgroundMap);
+		setSize(1200, 800);
+	}
+	
+	private void setInitLayout() {
+		setLayout(null);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+//		add(backgroundMap);
+	}
+	
+	private void addEventListener() {
+		
+	}
+
+	public static void main(String[] args) {
+		new StarwarsCalender();
+	}
+	
+}
