c4b03ef3b407e7e1dd5e0c5fef65f81cb74378c4
Nam-Cheol <namcher9428@gmail.com>
2024-06-03 23:36:45 +0900

    2024-06-03

--- src/starwarsCalender/StarwarsCalender.java
+++ src/starwarsCalender/StarwarsCalender.java
@@ -1,21 +1,23 @@
 package starwarsCalender;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.time.LocalDate;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
 
 public class StarwarsCalender extends JFrame {
 
 	private JLabel backgroundMap;
 
 	private TrayHandler trayHandler;
+	
+	private LocalDate today;
+	public int index;
 
 	// 월을 그리는 라벨
 	private JLabel month;
@@ -25,8 +27,6 @@ public class StarwarsCalender extends JFrame {
 	private JButton afterBtn;
 
 	// 월 정보에 대한 이미지
-	private String monthImg[] = { "Jan.png", "Feb.png", "Mar.png", "Apr.png", "May.png", "Jun.png", "Jul.png",
-			"Aug.png", "Sep.png", "Oct.png", "Nov.png", "Dec.png" };
 	private ImageIcon jan = new ImageIcon(getClass().getClassLoader().getResource("Jan.png"));
 	private ImageIcon feb = new ImageIcon(getClass().getClassLoader().getResource("Feb.png"));
 	private ImageIcon mar = new ImageIcon(getClass().getClassLoader().getResource("Mar.png"));
@@ -40,12 +40,15 @@ public class StarwarsCalender extends JFrame {
 	private ImageIcon nov = new ImageIcon(getClass().getClassLoader().getResource("Nov.png"));
 	private ImageIcon dec = new ImageIcon(getClass().getClassLoader().getResource("Dec.png"));
 
+	private ImageIcon monthImg[] = new ImageIcon[] {jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec};
+	
 	public StarwarsCalender() {
 		initData();
 		setInitLayout();
 		addEventListener();
 	}
 
+	
 	private void initData() {
 
 		backgroundMap = new JLabel(new ImageIcon(getClass().getClassLoader().getResource("starwarsBackground.png")));
@@ -55,9 +58,12 @@ public class StarwarsCalender extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setContentPane(backgroundMap);
 		setSize(1200, 800);
-
+		
+		today = LocalDate.now();
+		index = today.getMonthValue();
+		
 		trayHandler = new TrayHandler(this); // 트레이 핸들러 초기화
-		month = new JLabel(jan);
+		month = new JLabel(monthImg[index-1]);
 		beforeBtn = new JButton(new ImageIcon(getClass().getClassLoader().getResource("beforeBtn.png")));
 		afterBtn = new JButton(new ImageIcon(getClass().getClassLoader().getResource("afterBtn.png")));
 	}
@@ -74,13 +80,11 @@ public class StarwarsCalender extends JFrame {
 
 		beforeBtn.setSize(45, 50);
 		beforeBtn.setLocation(50, 20);
-		beforeBtn.setBorderPainted(false);
 		beforeBtn.setContentAreaFilled(false);
 		add(beforeBtn);
 
 		afterBtn.setSize(45, 50);
 		afterBtn.setLocation(330, 20);
-		afterBtn.setBorderPainted(false);
 		afterBtn.setContentAreaFilled(false);
 		add(afterBtn);
 	}
@@ -92,7 +96,26 @@ public class StarwarsCalender extends JFrame {
 //                trayHandler.minimizeToTray();
 //            }
 //        });
+		
+		beforeBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(index != 1) {
+					index -= 1;
+					month.setIcon(monthImg[index-1]);
+				}
+			}
+		});
 
+		afterBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(index != 12) {
+					index += 1;
+					month.setIcon(monthImg[index-1]);
+				}
+			}
+		});
 	}
 
 	public static void main(String[] args) {
